8807df44d29453e753d6b8e5b2583ab2f5ba0ee7
i.lysenko <i.lysenko@rollinggames.com>
2016-06-21 15:09:36 +0300

    Initial commit. Added login scene. Added ui maket, no logic yet.

---
+++ Directive901/Assets/Scripts/LoginPanelsUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginPanelsUI : MonoBehaviour {
+	[SerializeField] GameObject loginPanel,registerPanel;
+
+	void Start () {
+		btnShowLogin();
+	}
+
+	public void btnShowRegister()
+	{
+		loginPanel.SetActive(false);
+		registerPanel.SetActive(true);
+	}
+
+	public void btnShowLogin()
+	{		
+		loginPanel.SetActive(true);
+		registerPanel.SetActive(false);
+	}
+
+}
